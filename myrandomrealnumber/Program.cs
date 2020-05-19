bfde0364e7b8ad0b359d37fd65090b3f474bcf69
MoonHyekyung <moonsoft02@gmail.com>
2020-05-19 20:57:22 +0900

    do while이용, Random객체 이용하여 임의의 실수 맞추기

--- myrandomrealnumber/Program.cs
+++ myrandomrealnumber/Program.cs
@@ -10,6 +10,24 @@ namespace myrandomrealnumber
     {
         static void Main(string[] args)
         {
+            Random ran = new Random();
+            int nan = ran.Next(0, 101);
+            int input;
+
+            do
+            {
+                Console.Write("0부터 100사이의 숫자를 입력해주세요 : ");
+                input = int.Parse(Console.ReadLine());
+                if (input > nan)
+                {
+                    Console.WriteLine(input + "보다는 작은 숫자입니다.");
+                }else if(input < nan)
+                {
+                    Console.WriteLine(input + "보다는 큰 숫자입니다.");
+                }
+                Console.WriteLine();
+            } while (input != nan);
+            Console.WriteLine("정답입니다!");
         }
     }
 }
